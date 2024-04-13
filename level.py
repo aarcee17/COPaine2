094f6a983e23f69e792b8c1a7fdbbb957dd889fc
spurjhattam2207 <133047073+spurjhattam2207@users.noreply.github.com>
2024-04-13 08:27:19 +0530

    Hmm

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -14,6 +15,8 @@ class Level:
     
     def run(self):
         self.visible_sprites.draw(self.displaySurf)
+        self.visible_sprites.update()
+        debug(self.player.direction)
     
     def create_map(self):
         for row_idx, row in enumerate(WORLD_MAP):
@@ -21,4 +24,6 @@ class Level:
                 x = col_idx * TILESIZE
                 y = row_idx * TILESIZE
                 if col == 'x':
-                    Tile((x, y), [self.visible_sprites])
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
