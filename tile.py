27444a44cb6eb13575c315340c3d3b14a98bbd1b
spurjhattam2207 <133047073+spurjhattam2207@users.noreply.github.com>
2024-04-17 17:51:30 +0530

    map

--- tile.py
+++ tile.py
@@ -2,7 +2,8 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups,surface = pygame.Surface((TILESIZE,TILESIZE))):
         super().__init__(groups)
-        self.image = pygame.image.load('./graphics/test/rock.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
+        self.image = pygame.transform.scale(pygame.image.load(surface).convert_alpha(), (64, 64))
+        self.rect = self.image.get_rect(topleft = (pos[0],pos[1] - TILESIZE))
+        self.hitbox = self.rect.inflate(0,-10)
