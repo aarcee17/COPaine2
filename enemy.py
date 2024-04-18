ad74beb18daa38baa0075599ba20ad62440a57dd
spurjhattam2207 <133047073+spurjhattam2207@users.noreply.github.com>
2024-04-19 05:18:31 +0530

    Enemy almost done

--- enemy.py
+++ enemy.py
@@ -1,37 +1,42 @@
 import pygame
 from settings import *
+from basePlayer import BasePlayer
+import os
 
-class Entity(pygame.sprite.Sprite):
-    def __init__(self, groups):
+class Enemy(BasePlayer):
+    def __init__(self, pos, groups, obstacle_sprites, monster_info, damage_player):
         super().__init__(groups)
-        self.frame_index = 0
-        self.animation_speed = 0.15
-        self.direction = pygame.math.Vector2()
-
-    def move(self, speed):
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
-
-        self.hitbox.x += self.direction.x * speed
-        self.collision('horizontal')
-        self.hitbox.y += self.direction.y * speed
-        self.collision('vertical')
-        self.rect.center = self.hitbox.center
-
-    def collision(self, direction):
-        if direction == 'horizontal':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0:  # moving right
-                        self.hitbox.right = sprite.hitbox.left
-                    if self.direction.x < 0:  # moving left
-                        self.hitbox.left = sprite.hitbox.right
-
-        if direction == 'vertical':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0:  # moving down
-                        self.hitbox.bottom = sprite.hitbox.top
-                    if self.direction.y < 0:  # moving up
-                        self.hitbox.top = sprite.hitbox.bottom
-    
+        self.sprite_type = 'enemy'
+        
+        self.status = 'idle'
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -10)
+        self.obstacle_sprites = obstacle_sprites
+        
+        self.health = monster_info[0]
+        self.exp = monster_info[1]
+        self.speed = monster_info[2]
+        self.attack_damage = monster_info[3]
+        self.resistance = monster_info[4]
+        self.attack_radius = monster_info[5]
+        self.notice_radius = monster_info[6]
+        
+        self.animation = {
+            'idle': [],
+            'move': [],
+            'attack': []
+        }
+        
+        for animation in self.animation.keys():
+            self.animation[animation] = [file for file in os.walk('./graphics/monsters/{animation}')[2]]
+        
+    def get_player_distance_direction(self, player):
+        enemy_vec = pygame.math.Vector2(self.rect.center)
+        player_vec = pygame.math.Vector2(player.rect.center)
+        distance = (player_vec - enemy_vec).magnitude()
+        if distance > 0:
+            direction = (player_vec - enemy_vec).normalize()
+        else:
+            direction = pygame.math.Vector2()
+        return (distance, direction)
